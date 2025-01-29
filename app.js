81797cd7a8b6dffa5b12b8eab8448034e943ffb6
rob <robmathew21@gmail.com>
2025-01-29 11:08:34 -0600

    added event listeners for pause and play and added functions to handle this change

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ const ranges = player.querySelectorAll('.playerSlider');
 function togglePlay() {
     const method = video.paused ? 'play' : 'pause';
     video[method]();
+} 
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
 }
 
 // Hook up the event listeners 
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
